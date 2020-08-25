3be47726202c05684658331a9c23e65d78037ef4
rashien3 <hermanthong@gmail.com>
2020-08-25 17:55:25 +0800

    add echo and bye function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String level0 = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(level0);
+        String greeting = "    ____________________________________________________________\n" +
+                "    Hello! I'm Duke\n" +
+                "    What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+        System.out.println(greeting);
+
+        //level-1
+        String inputCommand = "a";
+        Scanner in = new Scanner(System.in);
+        while(!inputCommand.toLowerCase().equals("bye")){
+            inputCommand = in.nextLine();
+            System.out.println("    ____________________________________________________________\n" +
+                    "    " + inputCommand + "\n" +
+                    "    ____________________________________________________________");
+        }
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________");
     }
 }
