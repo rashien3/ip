523c5cd6aa670a711c2ba5b53415f27195addb29
rashien3 <hermanthong@gmail.com>
2020-08-25 18:17:05 +0800

    add add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,13 +10,30 @@ public class Duke {
         System.out.println(greeting);
 
         //level-1
-        String inputCommand = "a";
+        String inputCommand = "";
+        ArrayList<String> taskList = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
+        outerloop:
         while(!inputCommand.toLowerCase().equals("bye")){
             inputCommand = in.nextLine();
-            System.out.println("    ____________________________________________________________\n" +
-                    "    " + inputCommand + "\n" +
-                    "    ____________________________________________________________");
+            switch(inputCommand){
+            case "list":
+                int i = 0;
+                System.out.println("    ____________________________________________________________");
+                for(String task : taskList){
+                    System.out.println(++i + ". " + task);
+                }
+                System.out.println("    ____________________________________________________________");
+                break;
+            case "bye":
+                break outerloop;
+            default:
+                taskList.add(inputCommand);
+                System.out.println("    ____________________________________________________________\n" +
+                        "     added: " + inputCommand + "\n" +
+                        "    ____________________________________________________________");
+                break;
+            }
         }
         System.out.println("    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
