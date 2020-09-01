e5091a8283fae1836ec48c3e1cf2573dc8a3c208
rashien3 <hermanthong@gmail.com>
2020-09-01 13:24:28 +0800

    improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,35 +3,39 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "\t____________________________________________________________\n" +
+        static final String MESSAGE_GREETING = "\t____________________________________________________________\n" +
                 "\tHello! I'm Duke\n" +
                 "\tWhat can I do for you?\n" +
                 "\t____________________________________________________________\n";
-        System.out.println(greeting);
 
-        String inputCommand = "";
+        System.out.println(MESSAGE_GREETING);
+
         ArrayList<Task> taskList = new ArrayList<Task>();
         Scanner in = new Scanner(System.in);
+        String inputCommand = "";
         while(!inputCommand.toLowerCase().equals("bye")){
             inputCommand = in.nextLine();
             System.out.println("\t____________________________________________________________");
             switch(inputCommand){
-            case "list": // list out tasks
+            // list out tasks
+            case "list":
                 for(int i = 0; i < taskList.size(); i++){
                     Task t = taskList.get(i);
                     System.out.println( "\t" + (i+1) + ". " + "[" + t.getStatusIcon() + "] " + t.getDescription());
                 }
                 break;
-            case "bye": //break loop
+            //break loop
+            case "bye":
                 System.out.println("\tBye. Hope to see you again soon!");
                 break;
             default:
-                if(inputCommand.length() > 5 && inputCommand.substring(0,4).toLowerCase().equals("done")){ //set done
+                // done input
+                if(inputCommand.length() > 5 && inputCommand.substring(0,4).toLowerCase().equals("done")){
                     Task inputTask = taskList.get(Integer.parseInt(inputCommand.substring(5)) - 1);
                     inputTask.setDone(true);
                     System.out.println("\tNice! I've marked this task as done:\n" +
                             "\t[" + inputTask.getStatusIcon() + "] " + inputTask.getDescription());
-                }else{ // add task
+                }else{ // add input
                     taskList.add(new Task(inputCommand));
                     System.out.println("\tadded: " + inputCommand);
                 }
