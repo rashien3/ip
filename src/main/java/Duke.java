b0ee37deaddf20f4c540a7e15ec0f9e5963ac9a5
rashien3 <hermanthong@gmail.com>
2020-09-08 23:15:59 +0800

    add exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,16 @@ public class Duke {
             "What can I do for you?\n" +
             MESSAGE_LINEBREAK;
     private static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
-    private static final String MESSAGE_ERROR = "Please enter a valid command";
+    private static final String MESSAGE_ERROR_INVALID_COMMAND = " is not a valid command";
+    private static final String MESSAGE_ERROR_DONE = "ERROR: 'done' must be followed by an integer";
+    private static final String MESSAGE_ERROR_TODO = "ERROR: 'todo' must be followed by a desciption";
+    private static final String MESSAGE_ERROR_DEADLINE = "ERROR: 'deadline' must be followed by a desciption";
+    private static final String MESSAGE_ERROR_DEADLINE_MISSING_BY = "ERROR: 'deadline' must have a /by tag followed by a time";
+    private static final String MESSAGE_ERROR_EVENT = "ERROR: 'event' must be followed by a desciption";
+    private static final String MESSAGE_ERROR_EVENT_MISSING_AT = "ERROR: 'event' must have a /at tag followed by a time";
+    private static final String MESSAGE_ERROR_EMPTY_LIST = "ERROR: You have no tasks!";
+    private static final String TAG_BY = "/by ";
+    private static final String TAG_AT = "/at ";
     private static ArrayList<Task> taskList = new ArrayList<Task>();
     private static Scanner in = new Scanner(System.in);
     private static String inputCommand = "";
@@ -37,40 +46,95 @@ public class Duke {
         }
     }
 
-    public static void printList(){
+    public static void printList() throws DukeException {
+        if(taskList.size() == 0){
+            throw new DukeException();
+        }
+
         for(int i = 0; i < taskList.size(); i++) {
             Task t = taskList.get(i);
             System.out.println( (i+1) + ". " + t.toString());
         }
     }
 
-    public static void markDone(String inputCommand){
-        int taskNumber = Integer.parseInt(removeFirstWordOf(inputCommand)) - 1;
-        Task inputTask = taskList.get(taskNumber);
-        inputTask.setDone(true);
-        System.out.println("Nice! I've marked this task as done:\n\t" + inputTask.toString());
+    public static void markDone(String inputCommand) {
+        int taskNumber;
+        Task selectedTask;
+
+        try{
+            taskNumber = Integer.parseInt(removeFirstWordOf(inputCommand)) - 1;
+        } catch (NumberFormatException e) {
+            System.out.println(MESSAGE_ERROR_DONE);
+            return;
+        }
+
+        try{
+            selectedTask = taskList.get(taskNumber);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR: task '" + (taskNumber + 1) + "' does not exist");
+            return;
+        }
+
+        selectedTask.setDone(true);
+        System.out.println("Nice! I've marked this task as done:\n\t" + selectedTask.toString());
     }
 
-    public static void addTodo(String inputCommand){
-        Task t = new ToDo(removeFirstWordOf(inputCommand));
+    public static void addTodo(String inputCommand) throws DukeException {
+        String todoName = removeFirstWordOf(inputCommand);
+
+        if(todoName.equals("")){
+            throw new DukeException();
+        }
+
+        Task t = new ToDo(todoName);
         addTask(t);
     }
 
-    public static void addDeadline(String inputCommand){
-        int byIndex = inputCommand.indexOf("/by");
+    public static void addDeadline(String inputCommand) throws DukeException{
+        String description = "", by = "", deadlineCommand;
+        int byIndex;
 
-        String description = inputCommand.substring(9, byIndex - 1);
-        String by = inputCommand.substring(byIndex + 4);
+        deadlineCommand = removeFirstWordOf(inputCommand);
+        if(deadlineCommand.equals("") || deadlineCommand.equals(TAG_BY.trim())) {
+            throw new DukeException();
+        }
+
+        try {
+            byIndex = inputCommand.indexOf(TAG_BY);
+            description = inputCommand.substring(9, byIndex - 1);
+            by = inputCommand.substring(byIndex + 4);
+            if(by.equals("")) {
+                throw new DukeException();
+            }
+        } catch (StringIndexOutOfBoundsException | DukeException e){
+            System.out.println(MESSAGE_ERROR_DEADLINE_MISSING_BY);
+            return;
+        }
 
         Task t = new Deadline(description, by);
         addTask(t);
     }
 
-    public static void addEvent(String inputCommand){
-        int atIndex = inputCommand.indexOf("/at");
+    public static void addEvent(String inputCommand) throws DukeException {
+        String description = "", at = "", eventCommand;
+        int atIndex;
 
-        String description = inputCommand.substring(6, atIndex - 1);
-        String at = inputCommand.substring(atIndex + 4);
+        eventCommand = removeFirstWordOf(inputCommand).trim();
+        if(eventCommand.equals("") || eventCommand.equals(TAG_AT.trim())){
+            throw new DukeException();
+        }
+
+        try{
+            atIndex = inputCommand.indexOf(TAG_AT);
+            description = inputCommand.substring(6, atIndex - 1);
+            at = inputCommand.substring(atIndex + 4);
+            if(at.equals("")) {
+                throw new DukeException();
+            }
+        } catch (StringIndexOutOfBoundsException | DukeException e){
+            System.out.println(MESSAGE_ERROR_EVENT_MISSING_AT);
+            return;
+        }
 
         Task t = new Event(description, at);
         addTask(t);
@@ -82,33 +146,50 @@ public class Duke {
         System.out.println("Now you have " + (++numberOfTasks) + " tasks in the list.");
     }
 
-    public static void parseCommand(String inputCommand) throws DukeException {
+    public static void parseCommand(String inputCommand) {
+        String firstWord = getFirstWordOf(inputCommand);
+
         System.out.println(MESSAGE_LINEBREAK);
         try{
-            switch(getFirstWordOf(inputCommand)){
+            switch(firstWord){
             case "list":
-                printList();
+                try{
+                    printList();
+                } catch(DukeException e){
+                    System.out.println(MESSAGE_ERROR_EMPTY_LIST);
+                }
                 break;
             case "done":
                 markDone(inputCommand);
                 break;
             case "todo":
-                addTodo(inputCommand);
+                try{
+                    addTodo(inputCommand);
+                } catch(DukeException e) {
+                    System.out.println(MESSAGE_ERROR_TODO);
+                }
                 break;
             case "deadline":
-                addDeadline(inputCommand);
+                try{
+                    addDeadline(inputCommand);
+                } catch(DukeException e) {
+                    System.out.println(MESSAGE_ERROR_DEADLINE);
+                }
                 break;
             case "event":
-                addEvent(inputCommand);
+                try{
+                    addEvent(inputCommand);
+                } catch(DukeException e) {
+                    System.out.println(MESSAGE_ERROR_EVENT);
+                }
                 break;
             case "bye":
-                System.out.println(MESSAGE_BYE);
-                break;
+                return;
             default:
                 throw new DukeException();
             }
         } catch (DukeException e){
-            throw e;
+            System.out.println("'" + firstWord + "'" + MESSAGE_ERROR_INVALID_COMMAND);
         }
         System.out.println(MESSAGE_LINEBREAK);
     }
@@ -117,13 +198,8 @@ public class Duke {
         System.out.println(MESSAGE_GREETING);
 
         do {
-            try{
-                inputCommand = in.nextLine();
-                parseCommand(inputCommand);
-            } catch (DukeException e){
-                System.out.println(MESSAGE_ERROR);
-                System.out.println(MESSAGE_LINEBREAK);
-            }
+            inputCommand = in.nextLine();
+            parseCommand(inputCommand);
         } while (!inputCommand.equals("bye"));
 
         System.out.println(MESSAGE_BYE);
